2745c88c7cc21aa7996c7497da2d648fac8502b0
ABat <116818235+AnumBatool12@users.noreply.github.com>
2023-03-28 15:25:15 +0500

    Update Query10.sql
    Signed-off-by: ABat <116818235+AnumBatool12@users.noreply.github.com>

--- Part2/Query10.sql
+++ Part2/Query10.sql
@@ -2,5 +2,5 @@
 SELECT p.Name
 FROM Person p
 INNER JOIN Employee emp ON emp.PersonID=p.PersonID
-JOIN ServiceRecord sr ON sr.EmployeeID=emp.EmployeeID
+INNER JOIN ServiceRecord sr ON sr.EmployeeID=emp.EmployeeID
 WHERE sr.AirplaneRegNum = 'N123';
